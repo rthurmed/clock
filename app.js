6fff3e92f86f264e159a6d5e6bca90b8695ab349
Arthur Medeiros <rthurmedeiros@gmail.com>
2022-05-24 22:39:26 -0300

    Implemented dark/light mode system

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ function update () {
 
 setTimeout(update, timeout)
 
+// setup dark mode switcher
+const toggleDarkMode = () => {
+  document.body.classList.toggle('dark')
+  localStorage.setItem('dark', document.body.classList.contains('dark'))
+}
+
+document
+  .getElementById('dark-mode-switcher')
+  .onclick = toggleDarkMode
+
+// set dark mode if needed
+const enabledSystemDarkMode = (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+)
+
+if (enabledSystemDarkMode || localStorage.getItem('dark') === "true") {
+  toggleDarkMode()
+}
